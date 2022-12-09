456edac3abab4a7f14e82bb124acfd11a851d09d
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2022-12-09 19:26:21 +0200

    update contacts.js

--- contacts.js
+++ contacts.js
@@ -4,59 +4,53 @@ const { v4: uuidv4 } = require('uuid');
 const contactsPath = path.join(__dirname, 'db/contacts.json');
 
 function listContacts() {
-    return fs.readFile(contactsPath)
+  return fs.readFile(contactsPath)
     .then(data => {
       const contacts = JSON.parse(data);
       return contacts;
     })
     .catch(error => console.log(error.message));
-  };
+};
 
-  function getContactById(contactId) {
-    const contact = listContacts()
+function getContactById(contactId) {
+  const contact = listContacts()
     .then(contacts => contacts.find(item => item.id === contactId.toString()))
     .catch(error => console.log(error.message));
-    if(!contact){
-      return null;
-    }
-    return contact;
-  };
+  return contact || null;
+};
   
-  function removeContact(contactId) {
-    const removedContact = listContacts()
+function removeContact(contactId) {
+  const removedContact = listContacts()
     .then(contacts => contacts.find(item => item.id === contactId.toString()))
     .catch(error => console.log(error));
-    listContacts()
+  listContacts()
     .then(contacts => {
       const newContactList = contacts.filter(item => item.id !== contactId.toString());
-      fs.writeFile(contactsPath, JSON.stringify(newContactList));
+      fs.writeFile(contactsPath, JSON.stringify(newContactList, null, 2));
     })
     .catch(error => console.log(error.message));
-    if(!removedContact){
-      return null;
-    }
-    return removedContact;
-  };
+  return removedContact || null;
+};
   
-  function addContact(name, email, phone) {
-    const newContact = {
-      id: uuidv4(),
-      name: name,
-      email: email,
-      phone: phone,
-    };
-    const newContactList = listContacts()
+function addContact(name, email, phone) {
+  const newContact = {
+    id: uuidv4(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  const newContactList = listContacts()
     .then(contacts => {
       const newContacts = [...contacts, newContact];
-      fs.writeFile(contactsPath, JSON.stringify(newContacts));
+      fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
     })
     .catch(error => console.log(error.message));
-    return newContact;
-  };
+  return newContact;
+};
 
-  module.exports = {
-    listContacts,
-    getContactById, 
-    addContact,
-    removeContact
-  };
+module.exports = {
+  listContacts,
+  getContactById, 
+  addContact,
+  removeContact
+};
